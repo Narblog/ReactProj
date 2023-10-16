4cef37e3a23c584ba46cfa26fc162095c08e4399
Narek Grigoryan <ngrigoryan228@gmail.com>
2023-10-16 21:27:51 +0400

    installing React and React Dom

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import React from 'React';
+import ReactDOM from 'react-dom/client';
+
+
 import { NewProject } from "./test";
 NewProject()
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.innerHTML="<h1>Hello</h1>"
--- webpack.config.ts
+++ webpack.config.ts
@@ -25,7 +25,7 @@ const config:webpack.Configuration={
          })
     ],
     resolve:{
-        extensions:[".tsx",".ts",".js"]
+        extensions:[".ts",".tsx",".js"]
     }
 }
 
